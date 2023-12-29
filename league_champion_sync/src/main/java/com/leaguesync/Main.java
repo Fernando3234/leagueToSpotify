306c5ce7f357a5f2db744f72ce188ce6ff81aef6
Fernando3234 <45490982+Fernando3234@users.noreply.github.com>
2023-12-28 20:56:52 -0500

    Added functionality that leverages Riot API that pulls accounts data via username. Grabs current match data and extraxt what champion is being played.

--- league_champion_sync/src/main/java/com/leaguesync/Main.java
+++ league_champion_sync/src/main/java/com/leaguesync/Main.java
@@ -1,7 +1,23 @@
 package com.leaguesync;
 
+import org.json.simple.JSONArray;
+import org.json.simple.parser.ParseException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            run();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void run() throws ParseException {
+        RiotApi api = new RiotApi();
+        LeagueFetcher lf = new LeagueFetcher(api);
+        String summonerName = lf.getSummonerName();
+        String accountID = lf.grabAccountId(summonerName);
+        JSONArray matchData = lf.grabMatchParticipantsData(accountID);
+        System.err.println(lf.findPlayerChamp(matchData, summonerName));
     }
 }
--- league_champion_sync/src/main/java/com/leaguesync/leagueFetcher.java
+++ league_champion_sync/src/main/java/com/leaguesync/leagueFetcher.java
@@ -1,6 +1,14 @@
 package com.leaguesync;
 //Input:  League Of Legends API data 
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 //Output: ouputs a champion name
 
 public class LeagueFetcher {
@@ -10,12 +18,61 @@ public class LeagueFetcher {
         this.api = api;
     }
 
-    // create method that gets your account id
+    // purpose: grab Username from User via prompt
+    public String getSummonerName() {
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("What is your Riot Username?");
+        String summonerName = userInput.nextLine();
+        userInput.close();
+        return summonerName;
+    }
 
-    // create a method that accesses league match via account id
+    // purpose: grab acccount ID via the summoner name provided in get Summoner
+    // Name()
+    public String grabAccountId(String summonerName) throws ParseException {
+        JSONParser parser = new JSONParser();
+        JSONObject jsonObject;
+        String accountId;
 
-    // grabs the champion id
+        String accountInformation = api.makeApiRequest(
+                "https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + summonerName
+                        + "?api_key=RGAPI-d98bd254-8443-40dc-adba-c3f29e9d1444");
 
-    // create a method that returns the champion name via the champion id
+        jsonObject = (JSONObject) parser.parse(accountInformation);
 
+        accountId = (String) jsonObject.get("id");
+
+        return accountId;
+    }
+
+    // purpose: grab array holding all participants in a given match found by the
+    // account ID provided in grabAccountId()
+    public JSONArray grabMatchParticipantsData(String accountId) throws ParseException {
+        JSONParser parser = new JSONParser();
+        JSONObject matchData;
+        JSONArray participant;
+
+        String rawMatchData = api.makeApiRequest(
+                "https://na1.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/" +
+                        accountId + "?api_key=RGAPI-d98bd254-8443-40dc-adba-c3f29e9d1444");
+
+        matchData = (JSONObject) parser.parse(rawMatchData);
+
+        participant = (JSONArray) matchData.get("participants");
+
+        return participant;
+    }
+
+    // purpose: Using the Array provideed in grabMatchParticipantsData() find the
+    // users player and return the champion ID
+    public Object findPlayerChamp(JSONArray participants, String summonerName) throws ParseException {
+        Object player;
+        for (Object x : participants) {
+            player = ((JSONObject) x).get("summonerName");
+            if (summonerName.equals(player)) {
+                return ((JSONObject) x).get("championId");
+            }
+        }
+        return null;
+    }
 }
